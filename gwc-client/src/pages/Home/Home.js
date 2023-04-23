27262c837e07cea18960860ab7256bb9a88cd4e6
julachancey <julia.chancey@ufl.edu>
2023-04-23 19:23:46 -0400

    changed descriptions for build sisterhood, leadership, and ur careers

--- gwc-client/src/pages/Home/Home.js
+++ gwc-client/src/pages/Home/Home.js
@@ -1,34 +1,60 @@
-import React from 'react'
-import './Home.css'
-import NavBar from '../../components/Navbar'
-import { Button } from 'semantic-ui-react'
+import React from "react";
+import "./Home.css";
+import NavBar from "../../components/Navbar";
+import { Button } from "semantic-ui-react";
 
 const Home = () => {
   return (
     <div className="home">
-      <div class='ui inverted vertical masthead center aligned segment hero'>
-            <div class='ui text container'>
-                <h1 class='ui inverted header'>Girls Who Code @ UF</h1>
-                <h2>On a mission to close the gender gap in tech.</h2>
-                <button class='ui huge secondary button'>
-                    Learn More <i class='right arrow icon'></i>
-                </button>
-            </div>
+      <div class="ui inverted vertical masthead center aligned segment hero">
+        <div class="ui text container">
+          <h1 class="ui inverted header">Girls Who Code @ UF</h1>
+          <h2>On a mission to close the gender gap in tech.</h2>
+          <button class="ui huge secondary button">
+            Learn More <i class="right arrow icon"></i>
+          </button>
+        </div>
       </div>
-      
+
       <div className="mission-text">
         <div class="ui stackable relaxed three column grid container">
           <div class="column">
             <h2>BUILD SISTERHOOD</h2>
-            <h3>College & Career programs connect young women and non-binary people who are interested in tech to build a supportive community that helps them persist and succeed in the field.</h3>
+            <h3>
+              Join a community of like-minded women who share a passion for
+              coding and technology. Through Girls Who at UF, you'll have the
+              opportunity to connect with other students who are pursuing
+              careers in tech and build lifelong friendships. By working
+              together, sharing experiences, and supporting each other, you'll
+              be able to create a network of supportive sisters that will be
+              there for you throughout your career.
+            </h3>
           </div>
           <div class="column">
             <h2>BUILD LEADERSHIP</h2>
-            <h3>College & Careers offer students and young professionals opportunities to practice leadership and learn from peers and industry role models.</h3>
+            <h3>
+              Girls Who Code at UF empowers women to become leaders in
+              technology. Through our programs and events, you'll gain the
+              skills and knowledge needed to succeed in your career, and you'll
+              have the opportunity to develop your leadership skills. You'll
+              learn how to take charge, collaborate effectively, and inspire
+              others to achieve their goals. Whether you want to lead a team,
+              start your own business, or make a difference in your community,
+              we will provide you with the tools you need to succeed.
+            </h3>
           </div>
           <div class="column">
             <h2>BUILD YOUR CAREER</h2>
-            <h3>College & Career programs provide college-aged students and early career professionals with exclusive opportunities to build relationships with industry professionals, develop and practice key skills, and connect with Girls Who Code corporate partners for internships and jobs.</h3>
+            <h3>
+              Girls Who Code provides you with the resources and support you
+              need to succeed in your career. Through our programs, you'll gain
+              hands-on experience in coding and technology, and you'll have the
+              opportunity to work on real-world projects. You'll learn from
+              experienced professionals in the field, and you'll have access to
+              resources that will help you land your dream job. Whether you want
+              to work for a tech giant, a startup, or a nonprofit, Girls Who
+              Code at UF will help you get there.
+            </h3>
           </div>
         </div>
       </div>
@@ -41,7 +67,10 @@ const Home = () => {
               <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"></img>
             </div>
             <div class="content">
-              <div class="header">"This is a very long quote and a test to see how wrapping works."</div>
+              <div class="header">
+                "This is a very long quote and a test to see how wrapping
+                works."
+              </div>
               <div class="meta">
                 <a>Name</a>
                 <a>2nd year Computer Science major</a>
@@ -53,7 +82,10 @@ const Home = () => {
               <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"></img>
             </div>
             <div class="content">
-              <div class="header">"This is a very long quote and a test to see how wrapping works."</div>
+              <div class="header">
+                "This is a very long quote and a test to see how wrapping
+                works."
+              </div>
               <div class="meta">
                 <a>Name</a>
                 <a>2nd year Computer Science major</a>
@@ -65,7 +97,10 @@ const Home = () => {
               <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"></img>
             </div>
             <div class="content">
-              <div class="header">"This is a very long quote and a test to see how wrapping works."</div>
+              <div class="header">
+                "This is a very long quote and a test to see how wrapping
+                works."
+              </div>
               <div class="meta">
                 <a>Name</a>
                 <a>2nd year Computer Science major</a>
@@ -73,11 +108,9 @@ const Home = () => {
             </div>
           </div>
         </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
-
