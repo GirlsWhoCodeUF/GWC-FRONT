327a3a2033be06ca922024a78765c845b92205c9
MUNING CHEN <muning.chen@ufl.edu>
2023-09-04 16:07:01 -0400

    made a login page front end

--- gwc-client/src/pages/Login/Login.js
+++ gwc-client/src/pages/Login/Login.js
@@ -1,15 +1,41 @@
 import React from 'react'
+import './Login.css'
+import user_icon from '../../components/Assets/person.png'
+import email_icon from '../../components/Assets/email.png'
+import password_icon from '../../components/Assets/password.png'
 
 const Login = () => {
 
   return (
-    <>
-     <div style={{
-      color: 'white'
-    }}>Login page stuff </div>
-    </>
+    <div>
+      <div className='logincontainer'>
+        <div className='loginheader'>
+            <div className='logintext'>Sign Up</div>
+            <div className='loginunderline'></div>
+        </div> 
+        <div className='inputs'>
+            <div className='input'>
+                <img src={user_icon} alt="" />
+                <input type="text" />
+            </div>
+            <div className='input'>
+                <img src={email_icon} alt="" />
+                <input type="email" />
+            </div>
+            <div className='input'>
+                <img src={password_icon} alt="" />
+                <input type="password" />
+            </div>
+        </div>
+        <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
+        <div className="submit-container">
+            <div className="submit">Sign Up</div>
+            <div className="submit">Login</div>
+        </div>
+    </div>
+    </div>
     
-  )
+  );
 }
 
 export default Login
